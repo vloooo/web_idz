e65dd09b40733564f12c858357f9e55cc699e898
vloooo <43804299+vloooo@users.noreply.github.com>
2019-05-01 23:46:51 +0300

    Add files via upload
    Я УСПЕЛ!

---
+++ idz.js
@@ -0,0 +1,254 @@
+	            function change() {
+            // console.log(fetchfile("kino.html"));
+            var txt = [`
+        <div class="card-deck name-deck">
+
+
+  <div class="card card-names">
+    <img class="card-img-top" src="drcl.jpg" alt="Card image cap">
+
+    <div class="card-footer">
+      <small class="text-muted">Веселие</small>
+    </div>
+  </div>
+  <div class="card card-names">
+    <img class="card-img-top" src="jama.jpg" alt="Card image cap">
+
+    <div class="card-footer">
+      <small class="text-muted">Лучезарность</small>
+    </div>
+  </div>
+      <div class="card card-names">
+    <img class="card-img-top" src="listev.jpg" alt="Card image cap">
+
+    <div class="card-footer">
+      <small class="text-muted">Авантюризм</small>
+    </div>
+  </div>
+
+  <div class="card card-names">
+    <img class="card-img-top" src="tretiak.JPG" alt="Card image cap">
+
+    <div class="card-footer">
+      <small class="text-muted">Третьяк</small>
+    </div>
+  </div>
+
+</div>
+    <div class="w-100 vlad-wrap"><img class="vlad" src="vlad_w2.png" alt="Card image cap">
+    <img class="vlad-n" src="vlad_n.jpg"  alt="Card image cap"></div>`, 
+
+`
+                <div class="align-self-start col rcn-wrp"><img class="card col-sm-11 rcn-card" src="rcn1.jpg" alt="Card image cap">
+    <div class="carousel-caption">
+    	<h3 >happy</h3>
+    </div>
+    </div>
+
+    <div class="col rcn-wrp"><img class="card col-sm-11 rcn-card" src="rcn2.jpg" alt="Card image cap">
+    <div class="carousel-caption">
+    	<h3 >upset</h3>
+    </div>
+    </div>
+
+    <div class="align-self-end col rcn-wrp"><img class="card col-sm-11 rcn-card" src="rcn3.jpg" alt="Card image cap">
+    <div class="carousel-caption">
+    	<h3 >lazy</h3>
+    </div>
+        </div>`, 
+
+    `<div class="container-fluid"> <h3 class="text-center email">elektronnaya_pochta_eto_lichnaya_informaciya_poetomu_ya_ne_napishu_ee@google.com</h3></div>`, ` <div class="container-fluid"> <h3 class="text-center">Интересная информация о городе в котором я живу: <br> этот город называется Харьков. <br> я в нём живу.</h3></div> `, `	<blockquote class="blockquote text-center">
+  Спасибо, я уже покушал. Да, я не буду больше так смотреть на дверной косяк. Свеча зажглась.
+  <footer class="blockquote-footer"><cite title="da eto ya">vlad ovsianikov</cite></footer>
+</blockquote>`, `
+      <div class="w-100"><h3 class="h3-responsive">Таблица с оценками</h3></div>
+
+                                                                                     
+  <div class="table table-hover table-responsive">          
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Предмет</th>
+        <th>Преподователь</th>
+        <th>Получил</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td class="text-left">Технологии программирования</td>
+        <td class="text-left">Дружинин</td>
+        <td>А</td>
+      </tr>
+            <tr>
+        <td>2</td>
+        <td class="text-left">Анализ данных</td>
+        <td class="text-left">Мартыненко</td>
+        <td>А</td>
+      </tr>
+            <tr>
+        <td>3</td>
+        <td class="text-left">Моделирование</td>
+        <td class="text-left">мартыненко</td>
+        <td>А</td>
+      </tr>
+            <tr>
+        <td>4</td>
+        <td class="text-left">Методы исчисления</td>
+        <td class="text-left">Плаксий</td>
+        <td>А</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td class="text-left">Теория цип.</td>
+        <td class="text-left">Успенский</td>
+        <td>А</td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td class="text-left">Компьютерные сети</td>
+        <td class="text-left">Трубаев</td>
+        <td>А</td>
+      </tr>
+      <tr>
+        <td>7</td>
+        <td class="text-left">Ф.В.</td>
+		<td class="text-left">Рыбалко</td>
+        <td>А</td>
+      </tr>
+    </tbody>
+  </table>
+  </div>`, `  <!--Carousel Wrapper-->
+<div id="carousel-example-2" class="carousel slide carousel-fade col" data-ride="carousel">
+  <!--Indicators-->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-2" data-slide-to="1"></li>
+    <li data-target="#carousel-example-2" data-slide-to="2"></li>
+  </ol>
+  <!--/.Indicators-->
+  <!--Slides-->
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <div class="view">
+        <img class="d-block w-75 img-thumbnail" src="plt9.jpg"
+          alt="First slide">
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Induction cooktop #1</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="view">
+        <img class="d-block w-75 img-thumbnail" src="plt2.jpg"
+          alt="Second slide">
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Induction cooktop #2</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="view">
+        <img class="d-block w-75 img-thumbnail" src="plt8.jpg"
+          alt="Third slide">
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Induction cooktop #3</h3>
+      </div>
+    </div>
+  </div>
+  <!--/.Slides-->
+  <!--Controls-->
+  <a class="carousel-control-prev next-prev" href="#carousel-example-2" role="button" data-slide="prev">
+    <span class="carousel-control-prev" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next next-prev" href="#carousel-example-2" role="button" data-slide="next">
+    <span class="carousel-control-next" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <!--/.Controls-->
+</div>
+<!--/.Carousel Wrapper-->`, 
+
+
+`<div class="w-100"><img class="vlad" src="vladlogo2.png" alt="Card image cap"></div>
+<div class="card-columns">
+
+  <div class="card kino-card">
+    <img class="card-img-top" src="artist.jpg" alt="Card image cap">
+
+  </div>
+  <div class="card kino-card">
+    <img class="card-img-top" src="scot.jpg" alt="Card image cap">
+
+  
+  </div>
+      <div class="card kino-card">
+    <img class="card-img-top" src="fate.jpg" alt="Card image cap">
+
+  </div>
+
+  <div class="card kino-card">
+    <img class="card-img-top" src="vinsent.jpg" alt="Card image cap">
+
+  </div>
+
+  <div class="card kino-card">
+    <img class="card-img-top" src="demolition.jpg" alt="Card image cap">
+</div>
+  <div class="card kino-card">
+    <img class="card-img-top" src="izgoy.jpg" alt="Card image cap">
+
+  </div>
+</div>`, `<div class="blockquote">
+Меня можно описать тремя словами:
+<ol class="text-left"><li>Целеустремлённый</li><li>Ответственный</li><li>Влад</li></ol> 
+</div>`]
+    // console.log(Math.floor(Math.random() * 2))
+    x = parseInt(document.getElementById("hide").innerHTML);
+    console.log(x)
+document.getElementById("content").innerHTML = txt[x];
+document.getElementById("hide").innerHTML = x + 1
+if (parseInt(document.getElementById("hide").innerHTML) == 9) {document.getElementById("hide").innerHTML = 0
+}
+
+            };
+
+function night() {
+	        // Get HTML head element 
+    x = parseInt(document.getElementById("hide-mode").innerHTML);
+
+          var head = document.getElementsByTagName('HEAD')[0];  
+
+        // // Create new link Element 
+        var link = document.createElement('link'); 
+  
+        // // set the attributes for link element  
+        link.rel = 'stylesheet';  
+      
+        link.type = 'text/css'; 
+
+    if (x) {
+    	document.getElementById("panda").style.backgroundImage = "url('gn_pnd.png')"
+    	document.getElementById("panda").style.backgroundColor = "white"
+    	link.href = 'day.css';  
+
+    }
+    else {
+    	document.getElementById("panda").style.backgroundImage = "url('gn_pnd2.png')"
+    	document.getElementById("panda").style.backgroundColor = "black"
+        link.href = 'night.css';  
+    }
+            head.appendChild(link); 
+
+document.getElementById("hide-mode").innerHTML = x + 1
+if (parseInt(document.getElementById("hide-mode").innerHTML) == 2) {document.getElementById("hide-mode").innerHTML = 0
+}
+
+
+      
+  
+        // // Append link element to HTML head 
+};
